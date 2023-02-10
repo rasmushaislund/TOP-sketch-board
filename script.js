d340c70cf4fa912986a53e94747b972106f26d42
Rasmus H <99361520+rasmushaislund@users.noreply.github.com>
2023-02-10 12:39:34 +0100

    Add dynamic background and pixel coloring func.
    Bug concerning missing background coloring when grid resolution
    was changed was fixed. It was a problem with the gridPixels variable
    that should be re-defined by createGrid().
    Also, pen-coloring feature for each pixel was implemented as part of
    createGrid() function with the penColor variable being defined by user
    choice via new function.

--- script.js
+++ script.js
@@ -1,29 +1,101 @@
-// GRID CREATION
+
+// DOM VARIABLES
+// Set DOM-related variables for background-color feature
+let bckgColorSelected = document.querySelectorAll(".color-bckg");
+let whiteBackground = document.querySelector("#white-background");
+let redBackground = document.querySelector("#red-background");
+let greenBackground = document.querySelector("#green-background");
+let blueBackground = document.querySelector("#blue-background");
+let gridPixels = document.querySelectorAll(".row");
+let row;
+
+// Set DOM-related variables for pen-color feature
+let penColorSelected = document.querySelectorAll(".color-pen");
+let colorPickPen = document.querySelector("#color-pick-pop");
+let rainbowPen = document.querySelector("#rainbow-pen");
+let eraser = document.querySelector("#eraser");
+    // gridPixels will be re-used from background color section //
+
+
+// NON-DOM VARIABLES
+// Variable to store grid background color
+let gridBackgroundColor = window.getComputedStyle(whiteBackground).backgroundColor;
+
 // Variables for grid creation
 let grid = document.querySelector(".grid");
 
 // Slider position will update text-box and grid resolution via eventListener
 let slider = document.querySelector("#grid-slider");
 let sliderValue = document.querySelector(".resol-output");
+
+// Variables related to drawing/coloring feature
+let draw = false;
+let penColor = document.querySelector("#color-pick-pop").value;
+console.log(penColor);
+
+
+// Event listener for updating slider value
 slider.addEventListener("mouseup", updateSliderValue);
 
 
+// UPDATE SLIDER
 function updateSliderValue() {
     sliderValue.textContent = slider.value + " x " + slider.value;
     createGrid();
 }
 
+
 // RESETTING WITH REFRESH/PAGE-LOAD
-// Reset page when refreshing/loading
-document.getElementsByTagName("body").onload = resetSketchBoard();
-
-
 function resetSketchBoard() {
     updateSliderValue();
-
-
-    
+    resetSelectedBckg();
+    resetSelectedPen();
+    defaultPenColor();
+    defaultBackgroundColor();
 }
+
+// Features used for resetting page when loading
+function defaultPenColor() {
+    colorPickPen.style.boxShadow = "0 0 0 2px rgba(232, 234, 237, 1)";
+}
+
+function defaultBackgroundColor() {
+    whiteBackground.style.boxShadow = "0 0 0 2px rgba(232, 234, 237, 1)";
+}
+
+document.getElementsByTagName("body").onload = resetSketchBoard();
+
+//RESETTING SKETCH BOARD WITH CLEAR BUTTON
+let btnClear = document.querySelector(".btn-clear");
+btnClear.addEventListener("click", resetSketchBoard);
+
+// GRID CREATION AND COLORING
+// Create grid by creating columns and rows and append to .grid parent.
+function createGrid() {
+    removeAllChildren();
+    for (let i = 0; i < slider.value; i++) {
+        let column = document.createElement("div"); // Create column
+        column.className = "column";
+        for (let j = 0; j < slider.value; j++) {
+            let row = document.createElement("div"); // Create row
+            row.className = "row";
+            column.appendChild(row);
+
+            row.addEventListener("mouseover", function() { // Color pixel if draw is true
+                if (!draw) return;
+                row.style.backgroundColor = penColor;
+            })
+
+            row.addEventListener("mousedown", function() { // Color pixel on mouse down
+                row.style.backgroundColor = penColor;
+                })
+        }
+        grid.appendChild(column);
+        gridPixels = document.querySelectorAll(".row");
+    }
+    updateBackground();
+}
+
 
 
 // Remove all children from node .grid
@@ -33,43 +105,29 @@ function removeAllChildren() {
     }
 }
 
-// Create grid by creating columns and rows and append to .grid parent.
-function createGrid() {
-    removeAllChildren();
-    for (let i = 0; i < slider.value; i++) {
-        let column = document.createElement("div"); // create column
-        column.className = "column";
-        for (let j = 0; j < slider.value; j++) {
-            let row = document.createElement("div"); // create row
-            row.className = "row";
-            column.appendChild(row);
-        }
-        grid.appendChild(column);
-    }
-}
+// Setting draw to true on mousedown
+window.addEventListener("mousedown", function() {
+    draw = true;
+})
+
+// Setting draw to false on mouseup
+window.addEventListener("mouseup", function() {
+    draw = false;
+})
 
 
 // SET BACKGROUND COLOR
-// Set DOM-related variables
-let bckgColorSelected = document.querySelectorAll(".color-bckg");
-let whiteBackground = document.querySelector("#white-background");
-let redBackground = document.querySelector("#red-background");
-let greenBackground = document.querySelector("#green-background");
-let blueBackground = document.querySelector("#blue-background");
-let gridPixels = document.querySelectorAll(".row");
-
 // Add event listeners for all background color options
 whiteBackground.addEventListener("click", backgroundClicked);
 redBackground.addEventListener("click", backgroundClicked);
 greenBackground.addEventListener("click", backgroundClicked);
 blueBackground.addEventListener("click", backgroundClicked);
 
-// Add event listener for when setting new grid resolution
-slider.addEventListener("click", updateBackground);
-
-// Update grid background when slider is clicked
+// Update grid background color when grid is created or changed
 function updateBackground() {
-    
+    gridPixels.forEach(pixelRow => {
+        pixelRow.style.backgroundColor = gridBackgroundColor;
+    }) 
 }
 
 // Set box shadow highlight for selected background color and get the background color
@@ -77,8 +135,9 @@ function backgroundClicked(e) {
     resetSelectedBckg();
     let target = e.target;
     target.style.boxShadow = "0 0 0 2px rgba(232, 234, 237, 1)";
-    let selectedColor = window.getComputedStyle(target).backgroundColor;
-    colorBackground(selectedColor);
+    gridBackgroundColor = window.getComputedStyle(target).backgroundColor;
+    console.log(gridBackgroundColor)
+    colorBackground(gridBackgroundColor);
     }
 
 // Reset boxshadow for background color selection
@@ -89,21 +148,14 @@ function resetSelectedBckg() {
 }
 
 // The chosen and parsed background color is applied to the grid
-function colorBackground(selectedColor) {
+function colorBackground(gridBackgroundColor) {
     gridPixels.forEach(pixel => {
-        pixel.style.backgroundColor = selectedColor;
+        pixel.style.backgroundColor = gridBackgroundColor;
     })
 }
 
 
 // PEN COLOR FEATURE INCL. ERASER
-// Set DOM-related variables
-let penColorSelected = document.querySelectorAll(".color-pen");
-let colorPickPen = document.querySelector("#color-pick-pop");
-let rainbowPen = document.querySelector("#rainbow-pen");
-let eraser = document.querySelector("#eraser");
-    // gridPixels will be re-used from background color section //
-
 // Add event listeners for pen color options
 colorPickPen.addEventListener("click", penClicked);
 rainbowPen.addEventListener("click", penClicked);
